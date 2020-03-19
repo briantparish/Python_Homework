86a39d58a2e6d18dad96d27167cb378cb0e0fe0c
briantparish <60723557+briantparish@users.noreply.github.com>
2020-03-19 16:15:14 -0700

    Update main.py

--- pyBoss/main.py
+++ pyBoss/main.py
@@ -52,22 +52,40 @@ us_state_abbrev = {
     'Wyoming': 'WY',
 }
 
+#label input and output files
 input_file = 'employee_data.csv'
 output_file = 'employee_data_out.csv'
-with open(input_file, encoding='utf8') as input_csv, open(output_file,'w', newline='\n') as output_csv:
-    employee_data = csv.DictReader(input_csv, delimiter=",")
 
-    fieldnames =['Emp ID','Name','First Name','Last Name','DOB','SSN','State']
+#Open input reader and output writer
+with open(input_file, encoding='utf8') as input_csv, open(output_file,'w', newline='\n') as output_csv:
+    
+    #Read input CSV file and store as a list of dictionaries
+    employee_data = csv.DictReader(input_csv, delimiter=",")
+    
+    #Define dictionary keys for output
+    fieldnames =['Emp ID','First Name','Last Name','DOB','SSN','State']
+    
+    #Write desired output CSV dictionary keys as header
     employee_writer = csv.DictWriter(output_csv,fieldnames=fieldnames)
-    employee_writer.writeheader()
-    next(employee_data)     
+      
     for emp in employee_data:
+        #split DOB by '-' to format date in different order
         date = emp['DOB'].split("-")
         emp['DOB'] = date[1] + "/" + date[2] + "/" + date[0]
+        
+        #remove first 5 digits of SSN and include the last 4 using split
         emp['SSN'] = "xxx-xx-" + emp['SSN'].split("-")[2]
+        
+        #Use state abbreviation dictionary to replace State with abbreviation
         emp['State'] = us_state_abbrev[emp['State']]
+        
+        #split the name by a space to seperate into first and last
         name = emp['Name'].split(" ")
         emp['First Name'] = name[0]
         emp['Last Name'] = name[1]
-        del emp['Name']        
+        
+        #delete 'Name' key from dictionary
+        del emp['Name']
+        
+        #Finally write the formatted dictionary to output csv file
         employee_writer.writerow(emp)
