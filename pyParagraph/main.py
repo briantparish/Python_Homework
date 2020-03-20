12a332d269656f4aec441c25a3d45cf93e244a2b
briantparish <60723557+briantparish@users.noreply.github.com>
2020-03-20 10:05:29 -0700

    Update main.py

--- pyParagraph/main.py
+++ pyParagraph/main.py
@@ -2,40 +2,38 @@
 #Python Homework
 #pyParagraph
 
-
 import re
-import os
-
-wordcount = 0
-sentencecount = 0
-totalchars = 0
 
 #Create list of files to analyze
 input_files = ["paragraph_1.txt", "paragraph_2.txt"]
 
 for files in input_files:
+    wordcount = 0
+    sentencecount = 0
+    totalchars = 0
     with open(files,'r')  as file1:
         print("\nAnalyzing file: " + files + "...")
         for paragraph in file1:
-            #Count the number of sentences
             sentences = re.split("(?<=[.!?]) +", paragraph)
             if(sentences[0] != "\n"):
                 sentencecount += len(sentences)
             for sentence in sentences:
-                #split sentence by spaces to count words
+            #split sentence by spaces to count words
                 words = sentence.split(" ")
                 wordcount += len(words)
                 for chars in words:
                     #Don't count non-letter characters
                     chars = chars.replace(",","")
+                    chars = chars.replace("'","")
                     chars = chars.replace('"',"")
                     chars = chars.replace(".","")
+                    chars = chars.replace("\n","")
                     #count all characters in the words and sum them
                     totalchars += len(chars)
 
-        print(f"Paragraph Analysis\n" + \
-                "-----------------" + \
-                "\nApproximate Word Count: " + str(wordcount) + \
-                "\nApproximate Sentence Count: " + str(sentencecount) + \
-                "\nCharacters per word: " + str(round(totalchars/wordcount, 1)) + \
-                "\nAverage sentence length: " + str(round(wordcount/sentencecount, 1)))
+    print(f"\nParagraph Analysis\n" + \
+          "-----------------" + \
+          "\nApproximate Word Count: " + str(wordcount) + \
+          "\nApproximate Sentence Count: " + str(sentencecount) + \
+          "\nCharacters per word: " + str(round(totalchars/wordcount, 1)) + \
+          "\nAverage sentence length: " + str(round(wordcount/sentencecount, 1)))
