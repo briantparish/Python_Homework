54a5b595893ec0a8135d5787c2716f2e537295a1
briantparish <60723557+briantparish@users.noreply.github.com>
2020-03-19 21:44:24 -0700

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,24 +10,17 @@ votes = {"Khan": 0,
          "Correy": 0,
          "Li": 0,
          "O'Tooley": 0}
-count = 0
 
-with open('Resources/election_data.csv', encoding='utf8') as input_csv:
+with open('Resources/election_data.csv') as input_csv:
     polling_data = csv.reader(input_csv, delimiter=",")
     next(polling_data)
     for vote in polling_data:
-        count += 1
-        if(vote[2] == "Khan"):
-            votes['Khan'] += 1
-        elif(vote[2] == "Correy"):
-            votes['Correy'] += 1
-        elif(vote[2] == "Li"):
-            votes['Li'] += 1
-        elif(vote[2] == "O'Tooley"):
-           votes["O'Tooley"] += 1
+        votes[vote[2]] += 1
 
 #Find the winner of the election
 Winner = max(votes.items(), key=operator.itemgetter(1))[0]
+#total number of votes
+count = sum(votes.values())
 
 #Prepare output formatting
 output_text =   "\nELECTION RESULTS" + \
